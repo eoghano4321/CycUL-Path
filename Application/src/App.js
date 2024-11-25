852e58f78dcee0d23b71be13aed7c99d43e05960
eoghano4321 <eoghano4321@gmail.com>
2024-11-25 18:55:04 +0000

    feat: Add frontend application implementing Radar

---
+++ Application/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RadarMap from './Components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <RadarMap></RadarMap>
+    </div>
+  );
+}
+
+export default App;
---
+++ Application/src/Components/Map.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Radar from 'radar-sdk-js';
+
+import 'radar-sdk-js/dist/radar.css';
+
+class RadarMap extends React.Component {
+  componentDidMount() {
+    Radar.initialize('prj_test_pk_a38604f904532778b141e186b60184e3502a5e68');
+
+    // create a map
+    const map = Radar.ui.map({
+      container: 'map',
+      style: 'radar-default-v1',
+      center: [-6.2603, 53.3498],
+      zoom: 14,
+    });
+
+    Radar.ui
+  }
+
+  render() {
+    return (
+      <div id="map-container" style={{ width:' 100%', height: '100vh', position: 'absolute' }}>
+        <div id="map" style={{ height: '100%', position: 'absolute', width: '100%' }} />
+      </div>
+    );
+  }
+};
+
+export default RadarMap;
